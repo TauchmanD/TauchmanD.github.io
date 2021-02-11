469a65e2c769b850b15288aaeada83a48d75e92e
Denis Tauchman <denis.tauchman@pslib.cz>
2021-02-11 11:16:07 +0100

    fresh start

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 function init() {
-    let searchParam = new URLSearchParams(window.location.search);
-    let selected = searchParam.get("x");
-    obrázky = document.getElementsByTagName('img');
-    console.log(obrázky[selected]);
-    let misto = document.getElementById("obrázek");
-    misto.appendChild(obrázky[selected]);
+    
 }
 
 
