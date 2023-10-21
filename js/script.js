9e704ae04a00bb37359014bde6824e8ea0e7d917
Denis Tauchman <denis.tauchman@gmail.com>
2023-10-21 12:26:49 +0200

    more animations batchest

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
 const init = () =>{
     intro = document.getElementById("intro");
-    let message = "ahoj"
+    content = document.getElementsByClassName("content")[0];
+    let message = "Denis Tauchman"
     let len = message.length
     let i = 0
     let show = ""
     const introIn = setInterval(()=>{
         if(i<len){
             show += message[i]
-            intro.innerHTML = `{ ${show}_ }`
+            intro.innerHTML = `{${show}_}`
         }
         i++
         if (i > len){
+            intro.innerHTML = `{${show}}`
             clearInterval(introIn)
             intro.style = "animation: intro-anim 1.5s forwards;"
+            content.style = "animation: content-in 2.5s forwards;"
         }
-    }, 200);
+    }, 100);
 
 }
 document.addEventListener("DOMContentLoaded", init)
