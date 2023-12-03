3aa9235b796524579c9947178340819bd78f7773
Denis Tauchman <denis.tauchman@gmail.com>
2023-12-03 18:10:48 +0100

    nvm lmao

--- js/script.js
+++ js/script.js
@@ -14,10 +14,10 @@ const init = () =>{
         if (i > len){
             intro.innerHTML = `{${show}}`
             clearInterval(introIn)
-            intro.style = "animation: intro-anim 1.5s forwards;"
-            content.style = "animation: content-in 2.5s forwards;"
+            intro.style = "animation: intro-anim 1s forwards;"
+            content.style = "animation: content-in 1s forwards;"
         }
-    }, 100);
+    }, 50);
     const age = setInterval(()=>{
         let t = Date.now()
         let b = new Date(2002, 8, 14).getTime()
